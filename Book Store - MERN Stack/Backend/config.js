5d3179a73f501337ceba63932a6662af6992052b
Dhiyanah Liyaudeen <140206024+dhiyanahL@users.noreply.github.com>
2024-11-19 23:08:23 +0530

    Update config.js

--- Book Store - MERN Stack/Backend/config.js
+++ Book Store - MERN Stack/Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const MONGODB_URL = 'mongodb+srv://dbUser:@cluster0.dydvmo7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
+export const MONGODB_URL = 'mongodb+srv://dbUser:username@cluster0.dydvmo7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
